9c87615c04b65f519c33474c633d0585897aafbf
hayabusabusa <hayabusabusaSY@gmail.com>
2020-01-23 12:27:10 +0900

    Add assets

--- lib/utils/assets.dart
+++ lib/utils/assets.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 class Assets {
   static final AssetImage icDiamond = AssetImage('assets/ic_diamond.png');
+  static final AssetImage imgDiamondMountain = AssetImage('assets/img_diamond_mountain.png');
 }
